e5e7efbe81f1054a183aeb0cc3496a83248aa4f9
Edenilson Molina <edenilsonmolina2001@hotmail.com>
2025-12-01 14:19:25 -0600

    feat: add GitHub Actions workflow for deployment and update README and config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://edenilson-molina.github.io',
+  base: '/portafolio-dev/',
   vite: {
     plugins: [tailwindcss()]
   }
